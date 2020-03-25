75aa7cf6e0346313c4e5c71b45167abcb1535d28
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-25 02:29:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(array)
   i = 0 
   collection = []
   while i < array.length 
-    yield array[i]
+    collection << yield array[i]
     i += 1
   end 
-    array
+    collection
 end
 
